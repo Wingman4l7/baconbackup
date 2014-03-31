bbd31d136d0c296008cab3a657b0c794a6129020
Wingman4l7 <Wingman4l7@gmail.com>
2014-03-31 00:29:25 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 baconbackup
 ===========
 
+### About ###
 Node.js tool for archiving a Reddit user's comments.
+Tested as working on Node.js v0.10.26.
+
+### How to Install ###
+This requires [Node.js] and the [restler] HTTP client library.
+The former will have to be downloaded and installed.
+Once that is done, the latter can be installed via this command:
+    
+    npm install restler 
+
+[node.js]: http://nodejs.org/
+[restler]: https://github.com/danwrong/restler
+
+### How to Run ###
+Set the `user` variable to be the handle of the person whose comments you wish to archive.  To use, navigate to the installed directory and run this command:
+
+	node baconbackup.js
+    
+### Details ###
+
+Currently baconbackup defaults to archiving as many comments as possible *(the upper limit is 1000)*.  At a maximum of 100 comments per page request and a minimum of 2 seconds between page requests, the fastest baconbackup can run through a full backup is **20 seconds**.
+    
+### License ###
+While obviously open-source, I have yet to decide which license this should be under.  It will probably end up being under the [GPL] or some flavor of [Creative Commons].
+[GPL]: http://www.gnu.org/licenses/licenses.html
+[Creative Commons]: http://creativecommons.org/licenses/
+
+## Origins & Credit Due ##
+
+Credit goes to [AlliedEnvy] for always being ready with a clever project name.
+[AlliedEnvy]: https://github.com/AlliedEnvy
+
+### Donations ###
+Like this script?  You can send Bitcoin donations to: `1F7kfMNUNQy8e52RHnQAWYXeaYfzFqHJAZ`
+
+*Quick reference:* $1 USD is currently: <img src="http://btcticker.appspot.com/mtgox/1.00usd.png">
+
+Alternatively, you can use [Gittip](https://www.gittip.com/Wingman4l7/).
---
+++ baconbackup.js
@@ -0,0 +1,60 @@
+var rest = require('restler') // for HTTP requests -- npm install restler : https://github.com/danwrong/Restler/
+  , fs   = require('fs');       // for logging
+
+var baseURL = 'http://www.reddit.com/user/'
+  , user    = 'Wingman4l7'
+  , tail    = '/.json?'
+  , limit   = 'limit=100'; // set to maximum, to limit page requests; default is 25
+
+var pages = [];
+var nextPage = '';  // will initially return the first page of comments
+
+var fileName = '.html';
+
+function writeToFile() {
+	var date = new Date();
+	var comment = '';
+	var subreddit = '';
+	
+	var archive = {}; // initialize object
+	
+	for(var i = 0; i < pages.length; i++) {
+		for(var j = 0; j < pages[i].data.children.length; j++) {
+			// write comments to file
+			date.setTime(pages[i].data.children[j].data.created_utc * 1000);
+			subreddit = pages[i].data.children[j].data.subreddit;
+			comment = pages[i].data.children[j].data.body_html;
+			if (typeof comment != "undefined") {
+				comment = comment.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&');  // fix escaped characters <, >, &
+			}
+
+			fs.appendFileSync(user + fileName, '<font face="verdana" size="2">'); // deprecated!
+			fs.appendFileSync(user + fileName, date + '<BR>' 
+													+ subreddit + '<BR>'
+													+ '\n' + comment + '\n' + '<BR><BR>');
+			fs.appendFileSync(user + fileName, '</font>'); // deprecated!
+		}
+	}
+}
+
+function onComplete(DATA, res) {	
+	pages.push(DATA); // add page to array
+	nextPage = DATA.data.after;
+	console.log('next page: ' + nextPage);
+	if(nextPage != null) { // not on the last page
+		setTimeout(function(){
+			getPage();
+		}, 2000);  // 2 second minimum as per Reddit's "be nice and don't hammer our servers" API rule.
+	}
+	else {
+		console.log('==finished; page count: ' + pages.length);
+		writeToFile();
+		console.log('==done writing to file');
+	}
+}
+
+function getPage() { 
+	rest.get(baseURL + user + tail + limit + '&after=' + nextPage).once('complete', onComplete);
+}
+
+getPage();
